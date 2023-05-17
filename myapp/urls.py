b1c6d51e891e72b430290f43aec3f213d9b1da48
carlafaes <carlafaes18@hotmail.com>
2023-05-17 08:21:55 -0300

    accedemos a url/id

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from myapp import views
 
+
 urlpatterns=[
     path('',views.hello),
     path('about/',views.about),
     path('projects/',views.projects),
-    path('tasks/',views.tasks),
+    path('tasks/<int:id>',views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-from .models import Project
+from .models import Project,Task
+from django.shortcuts import get_object_or_404 ##devuelve una pagina 404 si el objeto no existe, en lugar de crear una detencion en el servidor
 
 # Create your views here.
 def hello(request):
@@ -13,5 +14,6 @@ def projects(request):
     projects = list(Project.objects.values())
     return JsonResponse(projects, safe=False)
 
-def tasks(request):
-    return HttpResponse("Task")
+def tasks(request,id):
+    task=get_object_or_404(Task, id=id)
+    return HttpResponse("Task: %s" % task.title)
